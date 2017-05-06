43282e8a4c8128bb8de14539f17ee23219bc2494
Centro Ático <centroatico@202C11E17168971.local>
2017-05-06 13:03:00 -0500

    primeramodificacion

--- instagram/instagramapp/views.py
+++ instagram/instagramapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'Registro.html')
+def Entrada(request):
+    return render(request, 'Entrada.html')
+def Inicio(request):
+    return render(request, 'Inicio.html')
